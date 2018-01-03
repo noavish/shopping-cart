f8e59334a5ae6c3edb86aaa2659f4827abcb8a3e
noavish <noavish1@gmail.com>
2018-01-03 23:14:20 +0200

    updated remove-item-from-cart and added comments

--- public/addItem.js
+++ public/addItem.js
@@ -1,20 +1,19 @@
+// Create new item object according to the ADD ITEM form and clear the form fields.
 $('.add-new-item').click(function () {
     var itemName = $(this).siblings("input[data-id='item-name']").val();
     var itemPrice = $(this).siblings("input[data-id='item-price']").val();
     var itemImage = $(this).siblings("input[data-id='item-image']").val();
     var newItem = {newItemName: itemName, newItemPrice: itemPrice, newItemImage: itemImage};
     addNewItem(newItem);
-    clearForm();
+    $('.new-item-input').children('.form-control').val('');
 });
 
-function addNewItem(newItem) {
+// Append new item to the store using Handlebars.
+function addNewItem (newItem) {
     var newItemSource = $('#new-item-template').html();
     var newItemTemplate = Handlebars.compile(newItemSource);
     var newHTML = newItemTemplate(newItem);
     $('.new-item').append(newHTML);
 }
 
-function clearForm () {
-    $('.new-item-input').children('.form-control').val('');
-}
 
