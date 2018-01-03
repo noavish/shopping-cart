3d45d2b4a04ee77c94831a019049235c44678ece
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2018-01-03 13:03:51 +0200

    js code

--- public/script.js
+++ public/script.js
@@ -10,15 +10,25 @@ var updateCart = function () {
   var itemTemplate = Handlebars.compile(itemSource);
   var newHTML = itemTemplate({items: cart});
   $('.cart-list').append(newHTML);
+  $('.cart-list').children('p').append(`<span><i class="fa fa-trash-o"></i></span>`);
   $('.total').text(sumUpCart());
 }
 
 var addItem = function (item) {
+  for(var i = 0; i < cart.length; i++) {
+    if (item.itemName === cart[i].itemName) {
+      cart[i].quantity += 1
+      return;
+    }
+}
+cart.push(item)
+}
+
   // TODO: Write this function. Remember this function has nothing to do with display. 
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-  cart.push(item);
-  console.log(cart);
-}
+
+    
+
 
 var clearCart = function () {
   // TODO: Write a function that clears the cart ;-)
@@ -30,7 +40,7 @@ var clearCart = function () {
 function sumUpCart () {
   var cartSum = 0;
   for (var i=0; i<cart.length; i++) {
-    cartSum += cart[i].itemPrice;
+    cartSum += cart[i].itemPrice * cart[i].quantity;
   }
   return cartSum;
 }
@@ -44,7 +54,7 @@ $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
   var itemName = $(this).parents('.item').data().name;
   var itemPrice = $(this).parents('.item').data().price;
-  var item = {itemName: itemName, itemPrice: itemPrice};
+  var item = {itemName: itemName, itemPrice: itemPrice, quantity: 1};
   addItem(item);
   updateCart();
 });
@@ -56,3 +66,12 @@ $('.clear-cart').on('click', function () {
 // update the cart as soon as the page loads!
 updateCart();
 
+
+// remove item button
+function removeBtn() {
+  var $rmButton = $('<button class="rmBtn"><i class="fa fa-trash-o"></i></button>')
+  $rmButton.appendTo($('.cart-list'))
+}
+
+
+// $('<input type="button"<i class="icon-fixed-width icon-trash"></i></>')
